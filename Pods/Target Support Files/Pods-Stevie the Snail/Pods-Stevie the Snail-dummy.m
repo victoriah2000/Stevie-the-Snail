f52d3ebff9d5c9176a0f282db6ccdd57f1b15864
Victoria Heric <victoriah_2000@yahoo.com>
2020-12-23 13:35:17 -0800

    commit pods removal

--- Pods/Target Support Files/Pods-Stevie the Snail/Pods-Stevie the Snail-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Stevie_the_Snail : NSObject
-@end
-@implementation PodsDummy_Pods_Stevie_the_Snail
-@end
--- Pods/Target Support Files/Pods-Stevie the SnailUITests/Pods-Stevie the SnailUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Stevie_the_SnailUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Stevie_the_SnailUITests
-@end
